3b230fe69f41784ca01268982fba8ee21e76c30c
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-12-05 19:04:25 +0100

    feat: allow to open aggr.md/@username with left-click

--- background.ts
+++ background.ts
@@ -1,7 +1,21 @@
-function openPage() {
-  browser.tabs.create({
-    url: "https://aggr.md/@aymericbeaumet/",
+// TODO: fetch from GitHub token
+const username = "aymericbeaumet";
+
+// TODO: make this configurable
+const pinned = true;
+
+async function openPage() {
+  const tabs = await browser.tabs.query({
+    currentWindow: true,
+    url: `https://aggr.md/@${username}/*`,
   });
+
+  if (tabs.length > 0) {
+    const tab = tabs[0];
+    return browser.tabs.update(tab.id, { active: true, pinned });
+  }
+
+  return browser.tabs.create({ url: `https://aggr.md/@${username}/`, pinned });
 }
 
 browser.browserAction.onClicked.addListener(openPage);
