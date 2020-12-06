3af1c2761d10f487f5291475bcb93ba886ae2d2e
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-12-06 23:37:59 +0100

    feat: display aggr feed popup when clicking on browser action

---
+++ src/popup.ts
@@ -0,0 +1,21 @@
+(async function () {
+  const res = await fetch("https://aggr.md/@aymericbeaumet");
+  const text = await res.text();
+
+  document.open();
+  document.write(text);
+  document.close();
+
+  document.addEventListener(
+    "click",
+    function (event) {
+      if (event.target.tagName !== "A") {
+        return;
+      }
+      event.preventDefault();
+      window.open(event.target.getAttribute("href"));
+      window.close();
+    },
+    { capture: true, once: true, passive: false }
+  );
+})();
